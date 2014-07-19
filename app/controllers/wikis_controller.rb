8c7e89d80f0a070a121744061f6487535745acf8
Warren <warren.kushner@gmail.com>
2014-07-19 17:06:59 -0400

    made update wikis view and controller

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,34 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    # raise
+    if @wiki.save
+      flash[:notice] = "Wiki was saved"
+      redirect_to  @wiki
+    else
+      flash[:error] = "Wiki was not saved"
+      render :new
+    end
+  end
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki was updated"
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again"
+      render :edit
+  end
+end
+
+
 end

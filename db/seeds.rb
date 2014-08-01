1cb38555459129cd02949f62c763f7cada0b4a35
Warren <warren.kushner@gmail.com>
2014-07-31 21:09:54 -0400

    Seeded user data and attached users to wikis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
 require 'faker'
 
+
+
+#creates users
+5.times do 
+  user = User.new(
+
+    name:   Faker::Name.name,
+    email:  Faker::Internet.email,
+    password:  Faker::Lorem.characters(10)
+    )
+  user.save
+end
+
+users = User.all
+
+
+
 #creates wikis
 Wiki.destroy_all
 50.times do 
   Wiki.create(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
     )
@@ -13,5 +31,13 @@ wikis = Wiki.all
 
 puts "Seeds finsihed"
 puts "#{Wiki.count} wikis created"
+
+User.first.update_attributes(
+ email: 'warren.kushner@gmail.com',
+ password: 'helloworld',
+  )
+puts "Seeds finished"
+puts "#{User.count} Users"
+puts "#{Wiki.count} Wikis"
 
 

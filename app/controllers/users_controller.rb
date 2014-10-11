1fbabd9b38dfe34dc554641aa9fd03e4be1c6e98
Warren <warren.kushner@gmail.com>
2014-10-11 16:49:25 -0400

    added collaborators to personal page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
      if current_user.id == params[:id].to_i
       @user = User.find(params[:id])
       @wikis = @user.wikis
-
+      @collaboration_wikis = @user.collaborations.pluck :wiki_id
       authorize @user
      else
          redirect_to root_path
